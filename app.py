7f12b438f8b716922f28836c9d46f885ac26a3c5
Manvanth M <4mh23cs083@gmail.com>
2026-04-27 01:47:01 +0530

    bus

--- app.py
+++ app.py
@@ -429,4 +429,5 @@ def toggle_attendance():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
